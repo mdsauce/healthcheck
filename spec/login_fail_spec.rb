af7e000fbf5c2acc1cdc013a447c8ecdf96d9b1f
mdsauce <max.dobeck@saucelabs.com>
2019-02-21 11:43:00 -0800

    Added sleep to make sure we get the alert.

--- spec/login_fail_spec.rb
+++ spec/login_fail_spec.rb
@@ -8,6 +8,8 @@ describe "Authentication" do
     @browser.text_field(id: 'password').set 'junk password'
     @browser.button(type: 'submit').click
 
+    sleep 1
+
     expect(@browser.div(class: 'alert')).to exist
   end
 end

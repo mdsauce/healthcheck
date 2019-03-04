923c9ddc7667be2bc27d34417b4d2a4bc534216f
mdsauce <max.dobeck@saucelabs.com>
2019-03-04 13:07:36 -0800

    Removed sleep.

--- spec/vdc-us-west/dashboard_spec.rb
+++ spec/vdc-us-west/dashboard_spec.rb
@@ -6,7 +6,6 @@ describe "US VDC Web UI" do
     @browser.text_field(id: 'username').set ENV['HEALTHCHECK_TEST_USER']
     @browser.text_field(id: 'password').set ENV['HEALTHCHECK_USER_PW']
     @browser.button(type: 'submit').click
-    sleep 6
 }
 
 it "can reach Automated Tests" do
